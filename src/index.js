3d8fd957b82ce4df9fd53f98a90cde7ddc12fbe6
Jack Collins <jackcollins@Jacks-MacBook-Pro.local>
2019-03-21 10:09:29 -0700

    comment out unfinished functions to allow uploading to netlify

--- src/index.js
+++ src/index.js
@@ -1,19 +1,19 @@
 import "./styles.scss"
 import "./backend.js"
 
-
-$(document).ready(function(){
-  $("#form").submit(function(event){
-    event.preventDefault();
-    const input = $("#name").val();
-
-    //api call with user input
-    promise.then(function(response){
-      let body = JSON.parse(response);
-      $(".output").text(body)
-    })
-    function(error){
-      $(".errors").text(`${error.message}`)
-    }
-  })
-})
+// 
+// $(document).ready(function(){
+//   $("#form").submit(function(event){
+//     event.preventDefault();
+//     const input = $("#name").val();
+//
+//     //api call with user input
+//     promise.then(function(response){
+//       let body = JSON.parse(response);
+//       $(".output").text(body)
+//     })
+//     function(error){
+//       $(".errors").text(`${error.message}`)
+//     }
+//   })
+// })
